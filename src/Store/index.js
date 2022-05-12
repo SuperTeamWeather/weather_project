aaabc362671702301c90baf2ed4000fa1889ed08
kirill <kmeleshkin@mail.ru>
2022-05-12 20:35:53 +0700

    убрал комментарий window.__REDUX_DEVTOOLS_EXTENSION_

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,10 +1,10 @@
-import { createStore, combineReducers, compose, applyMiddleware } from "redux";
-import { currentUserDataReducer } from "./CurrentUserDataReducer/reducer";
-import { profilesDataReducer } from "./ProfiilesDataReducer/reducer";
-import { weatherReducer } from "./WeatherReducer/reducer";
-import { persistStore, persistReducer } from "redux-persist";
+import {createStore, combineReducers, compose, applyMiddleware} from "redux";
+import {currentUserDataReducer} from "./CurrentUserDataReducer/reducer";
+import {profilesDataReducer} from "./ProfiilesDataReducer/reducer";
+import {weatherReducer} from "./WeatherReducer/reducer";
+import {persistStore, persistReducer} from "redux-persist";
 import createSagaMiddleware from "@redux-saga/core"
-import { rootWatcherSaga } from "./sagas";
+import {rootWatcherSaga} from "./sagas";
 import storage from "redux-persist/lib/storage";
 
 const persistConfig = {
@@ -12,7 +12,6 @@ const persistConfig = {
     storage,
     blacklist: ["currentUserDataReducer", "weatherReducer"],
 }
-
 
 const rootReducer = combineReducers({
     profilesDataReducer,
@@ -24,11 +23,10 @@ const saga = createSagaMiddleware()
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-
 export const store = createStore(persistedReducer, compose(
     applyMiddleware(
         saga
-    ), //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    ), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ))
 
 saga.run(rootWatcherSaga)
